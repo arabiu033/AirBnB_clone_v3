bb745bbfe408d92d8a46d9a0f25cf6e5a6b616f1
Aminu Rabiu <arabiu033@gmail.com>
2023-02-22 20:45:38 +0100

    Continuation from v2 repo

---
+++ models/base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Base Model Module """
+import models
+from datetime import datetime
+from uuid import uuid4
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
+
+class BaseModel:
+    """
+    parent class to take care of the initialization,
+    serialization and deserialization of your future instances
+    """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize instances attributes
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(str(value), "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """
+        print: [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict.pop("_sa_instance_state", None)
+        return obj_dict
