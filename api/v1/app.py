276df23f68652021e2db469a29b5443d79a1b8e2
Aminu Rabiu <arabiu033@gmail.com>
2023-02-22 23:43:42 +0100

    Fix

---
+++ api/v1/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Status of our API """
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask import Flask, make_response, jsonify
+from flask import Blueprint
+from flask_cors import CORS
+
+"""Host and port env variables"""
+host_env = getenv('HBNB_API_HOST') or '0.0.0.0'
+port_env = getenv('HBNB_API_PORT') or 5000
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+""" Cors access to selected resources from a different origin."""
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """ Close db session """
+    storage.close()
+
+if __name__ == "__main__":
+    """
+    Main function
+    """
+    app.run(
+            host=host_env, port=port_env,
+            debug=True, threaded=True,
+    )
